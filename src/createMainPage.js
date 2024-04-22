03e57e951a37bc809f94b85885be146fbc66ef76
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-04-22 23:35:47 +0900

    Heading now displays

--- src/createMainPage.js
+++ src/createMainPage.js
@@ -1,5 +1,5 @@
 //Creating the Root Homepage
-function createMainpage() {
+function createMainPage() {
   createHeader();
 }
 //Creates and attaches the Header
@@ -8,15 +8,15 @@ function createHeader() {
   const mainContainer = document.querySelector(".mainContainer");
   const headerContainer = document.createElement("div");
   headerContainer.classList.add("headerContainer");
-  headerContainer.setAttribute("headerContainer");
+  headerContainer.setAttribute("id", "headerContainer");
   mainContainer.appendChild(headerContainer);
 
   //Add Visual Elements
   const heading = document.createElement("h1");
-  heading.classList("h1");
-  heading.setAttribute("h1");
+  heading.classList.add("h1");
+  heading.setAttribute("id", "h1");
   heading.textContent = "To Do List";
   headerContainer.appendChild(heading);
 }
 
-export { createMainpage };
+export { createMainPage };
